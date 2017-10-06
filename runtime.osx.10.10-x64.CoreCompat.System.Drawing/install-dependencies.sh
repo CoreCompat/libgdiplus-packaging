25e85084072ca1fe0c1e814cd10b318ce103c652
Frederik Carlier <frederik.carlier@quamotion.mobi>
2017-10-06 12:40:04 +0200

    Update libtiff and giflib instead of installing them on Travis

--- runtime.osx.10.10-x64.CoreCompat.System.Drawing/install-dependencies.sh
+++ runtime.osx.10.10-x64.CoreCompat.System.Drawing/install-dependencies.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-brew install autoconf automake libtool pkg-config glib cairo freetype fontconfig libpng libtiff giflib
-
+# Libtiff and giflib are preinstalled on Travis servers, so we need to do an update of those
+brew update
+brew update libtiff giflib
+brew install autoconf automake libtool pkg-config glib cairo freetype fontconfig libpng
