22870b8098fc06baa6c7deca98d03840bb6b1b0c
Frederik Carlier <frederik.carlier@quamotion.mobi>
2019-06-24 14:18:11 +0200

    Use corefx master (e.g. version 3.0)

---
+++ CoreCompat.System.Drawing.Tests/PlatformApis.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System
+{
+    class PlatformApis
+    {
+        public static string GetOSVersion()
+        {
+            return "1.0.0";
+        }
+
+        public static string GetOSName()
+        {
+            return Environment.OSVersion.ToString();
+        }
+    }
+}
---
+++ CoreCompat.System.Drawing.Tests/SkipTestException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Microsoft.DotNet.XUnitExtensions
+{
+    class SkipTestException : Exception
+    {
+        public SkipTestException()
+        {
+        }
+
+        public SkipTestException(string message) : base(message)
+        {
+        }
+
+        public SkipTestException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected SkipTestException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
